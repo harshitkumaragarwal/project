2905409c997fb50e058d884a60a722cf7ab4f1cb
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-08-24 23:08:52 +0530

    first commit

---
+++ calculater.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+void main(){
+    float num1,num2,result;
+    char operator,error;
+
+
+    printf("enter num1:");
+    scanf("%f",&num1);
+
+    printf("enter num2:");
+    scanf("%f",&num2);
+
+
+    printf("enter operator:");
+    scanf(" %c",&operator);
+
+    if (operator=='+'){
+    result=num1+num2; 
+    printf("result=%f\n", result);  
+ }
+    else if (operator=='-'){
+        result=num1-num2;
+
+        printf("result=%f\n", result);
+    }
+
+    else if (operator=='*') {
+        result=num1*num2;
+
+        printf("result=%f\n", result);
+    }
+
+    else if (operator='/'){ 
+
+        if (num2==0){
+        printf("Invalid Input");
+        }
+        else {
+         result=num1/num2;
+
+         printf("result=%f\n", result);
+        }
+          
+        }
+
+       else {
+        printf("Invalid operator\n");
+    }
+      
+
+}
