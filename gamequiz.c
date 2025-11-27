917c92d751064b61fbc745e349d227a6c462b95b
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-11-27 22:49:48 +0530

    project

--- gamequiz.c
+++ gamequiz.c
@@ -26,15 +26,50 @@ struct question {
   //  function declaration
 void startQuiz(struct question q[], int total, char topic[], char name[]);
 
+void startQuiz(struct question q[], int total, char topic[], char name[]) {
+
+    printf(blue "\n========== QUIZ STARTED ==========\n" reset);
+
+    printf(green "Topic %s\n" reset,topic);
+
+    printf(blue "======================\n"reset);
+
+    for(int i=0;i<total;i++){
+
+        printf(yellow "%d. %s\n" reset,i+1,q[i].question);
+
+        for(int option=0;option<4;option++)
+        printf(" %d %s\n",option +1,q[i].options[option]);
+
+        int ans;
+
+        printf("enter your anser (1 to 4)");
+        scanf("%d",&ans);
+
+        if(ans==q[i].correct)
+        printf(green"correct\n" reset);
+
+        else
+        printf(red "wrong!\n"reset);
+
+    }
+
+    printf(blue "\n==============QUIZ ENDED ============\n" reset);
     
- 
+}
+
+
+
+
 // this strcut is for programming purpose
 int main(){
     srand(time(0));
+    
+    
 
      struct question cprog[TOTAL_QUESTION] = { 
         {"Which header file is used for input and output?", 
-            {"stdio.h","string.h","time.h","stdlib.h"}, 1},
+            {"stdio.h","string.h","time.h","stdlib.h"},1},
 
         {"Which keyword is used to declare a constant variable in C?", 
             {"int","const","string","int main"}, 2},
@@ -97,27 +132,62 @@ int main(){
     };
     
 
-    char name[50];
+    char name[50], branch[50];
     int choice;
     int sapid;
+    int startoption;
 
-    
+     printf(blue "\n========== QUIZ PORTAL ==========\n" reset); // at the interference
 
-    printf("enter your name"); // enter your name
+     printf("Welcome to the Quiz Portal!\n");
+
+     printf("Rules:\n");
+
+     printf("1) Each correct answer = 1 point\n");
+
+     printf("2) No negative marking\n");
+
+     printf("3) You can view a hint if needed\n");
+ 
+     printf("4) Answer only in numbers 1-4\n");
+
+
+
+    printf( green"enter your name" reset); // enter your name
+
     scanf("%s",name);
 
-    pritf("enter your psid");
+    printf( green"enter your psid" reset);
     scanf("%d",&sapid);
+
+    printf( green"enter your branch" reset);
+    scanf("%s",branch);
+
+     printf(blue "\n==================================\n" reset);
+
+    printf(yellow "Press 1 to START the Quiz: " reset);
+
+    scanf("%d", &startoption);
+
+    if(startoption != 1) {
+        printf(red "Quiz not started. Exiting...\n" reset);
+        return 0;
+    }
+
+
  
     printf("choose your topic"); // topic you want to chosse option 
 
     printf("1) c programming\n"); // first is programming
 
-    printf("2) physic quiz"); // second is physic quiz
+    printf("2) physic quiz\n"); // second is physic quiz
 
     printf("enter your choice"); // choose from given two
 
     scanf("%d",&choice);
+
+
+
 
     if(choice==1)
         startQuiz(cprog,TOTAL_QUESTION,"C programming",name);// if choose 1 then c programming
@@ -132,4 +202,5 @@ int main(){
 
         
 }
+
 
